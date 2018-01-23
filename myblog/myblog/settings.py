e8321a3c8933c8a244eefd103fb551f0ad5e2dd9
toksan <t.moriya.ct@gmail.com>
2018-01-23 22:48:16 +0900

    os.path.join(BASE_DIR, 'static') にパス修正
    './static' でもOKだが最も適切な方法を学習するため

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['./static']  # os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # './static'
 
 # Auth
 # https://docs.djangoproject.com/en/2.0/ref/settings/#login-redirect-url
